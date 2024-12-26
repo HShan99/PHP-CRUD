b6616cefc22d836c2d0c894c663fb18ad9770f0b
HShan99 <shanharsharathnayake@gmail.com>
2024-12-26 21:20:02 +0530

    Create bd connection

---
+++ config.php
@@ -0,0 +1,17 @@
+<?php
+$dbhost="localhost";
+$dbuser="root";
+$dbpassword="";
+$dbname="php_crud";
+
+
+$connection = mysqli_connect($dbhost,$dbuser,$dbpassword,$dbname);
+
+if(!$connection){
+    die("Could not connect to database".mysqli_connect_errno());
+}
+else{
+    echo("Connected successfully to database");
+}
+
+?>
--- index.php
+++ index.php
@@ -10,34 +10,34 @@
 </head>
 
 <body class="bg-dark">
-    <!-- <div class="container"> -->
-    <!-- <div class="row p-2"> -->
-    <div class="col-1g-4 offset-lg-4 bg-light text-dark m-4 p-5 rounder" style="width: 1000px; height: 500px;">
-        <h3 class="text-center p-2">Insert Data Into Database</h3>
-        <hr class="bg-light">
-        <form action="" method="POST">
-            <div class="form-group">
-                <label for="name">Name</label>
-                <input type="text" name="name" class="form-control" placeholder="Name" required>
+    <div class="container">
+        <div class="row p-5 m-5">
+            <div class="col-1g-4 offset-lg-4 bg-light text-dark m-4 p-5 rounder" style="width: 1000px; height: 500px;">
+                <h3 class="text-center p-2">Insert Data Into Database</h3>
+                <hr class="bg-light">
+                <form action="" method="POST">
+                    <div class="form-group">
+                        <label for="name">Name</label>
+                        <input type="text" name="name" class="form-control" placeholder="Name" required>
+                    </div>
+                    <div class="form-group">
+                        <label for="email">Email</label>
+                        <input type="email" name="email" class="form-control" placeholder="Email" required>
+                    </div>
+                    <div class="form-group">
+                        <label for="name">Phone</label>
+                        <input type="tel" name="name" class="form-control" placeholder="Phone" required>
+                    </div>
+                    <div class="form-group">
+                        <label for="name">City</label>
+                        <input type="text" name="city" class="form-control" placeholder="City" required>
+                    </div>
+                    <div class="form-group">
+                        <input type="submit" class="btn btn-primary btn-block m-2" value="Insert">
+                    </div>
+                </form>
             </div>
-            <div class="form-group">
-                <label for="email">Email</label>
-                <input type="email" name="email" class="form-control" placeholder="Email" required>
-            </div>
-            <div class="form-group">
-                <label for="name">Phone</label>
-                <input type="tel" name="name" class="form-control" placeholder="Phone" required>
-            </div>
-            <div class="form-group">
-                <label for="name">City</label>
-                <input type="text" name="city" class="form-control" placeholder="City" required>
-            </div>
-            <div class="form-group">
-                <input type="submit" class="btn btn-primary btn-block m-2" value="Insert">
-            </div>
-        </form>
-    </div>
-    </div>
+        </div>
     </div>
 
 </body>
